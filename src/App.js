90e004beceb940a98a6b7b3f4e9301ff0308ccce
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-04-23 04:53:50 +0100

    feat: source data api :1234:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  });
   return (
     <div className='container m-auto p-4'>
       <div className='max-w-sm rounded overflow-hidden shadow-lg'>
