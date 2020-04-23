82ed3b0638a87500dd2ae20c84433ddfdfce0ebe
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-04-23 05:23:07 +0100

    refactor: create  img card  component:recycle:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import ImageCard from './components/ImageCard';
+
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -16,42 +18,13 @@ function App() {
       })
       .catch(err => console.log(err));
   });
+
   return (
-    <div className='container m-auto p-4'>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-        <img
-          src='https://source.unsplash.com/random'
-          alt=''
-          className='w-full'
-        />
-        <div className='px-6 py-4'>
-          <h3 className='text-3xl mb-2'>Photo by John Doe</h3>
-          <ul>
-            <li>
-              <strong>Views: </strong>
-              4000
-            </li>
-            <li>
-              <strong>Downloads: </strong>
-              300
-            </li>
-            <li>
-              <strong>Likes: </strong>
-              400
-            </li>
-          </ul>
-        </div>
-        <div className='px-6 py-4'>
-          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-            tag1
-          </span>
-          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-            tag2
-          </span>
-          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-            tag3
-          </span>
-        </div>
+    <div className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-4'>
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
